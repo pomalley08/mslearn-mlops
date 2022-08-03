d1fe748e86436b6b89528916bed0ffc1cab390a4
pomalley08 <pomalley08@users.noreply.github.com>
2022-08-03 17:47:29 +0000

    refactor experiment code to script

--- src/model/train.py
+++ src/model/train.py
@@ -1,5 +1,5 @@
 # Import libraries
-
+import mlflow
 import argparse
 import glob
 import os
@@ -11,8 +11,8 @@ from sklearn.linear_model import LogisticRegression
 
 # define functions
 def main(args):
-    # TO DO: enable autologging
-
+    # enable autologging
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -33,7 +33,10 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
+def split_data(df):
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+
+    return train_test_split(X, y, test_size=0.30, random_state=0)
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
