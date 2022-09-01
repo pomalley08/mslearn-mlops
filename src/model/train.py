91acef5cb4d777b0c9b52799f2dd50e1083299a3
pomalley08 <pomalley08@users.noreply.github.com>
2022-09-01 20:53:31 +0000

    fix indenting

--- src/model/train.py
+++ src/model/train.py
@@ -35,13 +35,13 @@ def get_csvs_df(path):
 
 def split_data(df):
     X, y = df[['Pregnancies',
-        'PlasmaGlucose',
-        'DiastolicBloodPressure',
-        'TricepsThickness',
-        'SerumInsulin',
-        'BMI',
-        'DiabetesPedigree',
-        'Age']].values, df['Diabetic'].values
+               'PlasmaGlucose',
+               'DiastolicBloodPressure',
+               'TricepsThickness',
+               'SerumInsulin',
+               'BMI',
+               'DiabetesPedigree',
+               'Age']].values, df['Diabetic'].values
 
     return train_test_split(X, y, test_size=0.30, random_state=0)
 
